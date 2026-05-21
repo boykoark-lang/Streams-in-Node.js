4fae527622f2d8d27d506fca7276c3768fd1d5a8
boykoark-lang <boyko.ark@e-u.edu.ua>
2026-05-22 01:28:52 +0300

    Implement file streaming server in stream_response.js

--- stream_response.js
+++ stream_response.js
@@ -1,1 +1,31 @@
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
 
+const PORT = process.argv[2] || 3000;
+
+const server = http.createServer((req, res) => {
+  const parsedUrl = url.parse(req.url, true);
+
+  if (req.method === 'GET' && parsedUrl.pathname === '/file') {
+    const fileName = parsedUrl.query.fileName;
+
+    if (!fileName || !fs.existsSync(fileName)) {
+      res.statusCode = 400;
+      return res.end();
+    }
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+
+    const readStream = fs.createReadStream(fileName);
+
+    readStream.pipe(res);
+
+  } else {
+    res.statusCode = 404;
+    res.end();
+  }
+});
+
+server.listen(PORT);
